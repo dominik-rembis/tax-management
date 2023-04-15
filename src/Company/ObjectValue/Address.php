05da97d12b09b6d3a34ba417302ae9b7067a1517
Dominik Rembis <dominik.rembis@gmail.com>
2023-04-15 17:35:04 +0200

    Address refactor

--- src/Company/ObjectValue/Address.php
+++ src/Company/ObjectValue/Address.php
@@ -7,29 +7,9 @@ namespace App\Company\ObjectValue;
 class Address
 {
     public function __construct(
-        private readonly string $street,
-        private readonly string $zipCode,
-        private readonly string $city,
-        private readonly string $country
+        public readonly string $street,
+        public readonly string $zipCode,
+        public readonly string $city,
+        public readonly string $country
     ) {}
-
-    public function getStreet(): string
-    {
-        return $this->street;
-    }
-
-    public function getZipCode(): string
-    {
-        return $this->zipCode;
-    }
-
-    public function getCity(): string
-    {
-        return $this->city;
-    }
-
-    public function getCountry(): string
-    {
-        return $this->country;
-    }
 }
